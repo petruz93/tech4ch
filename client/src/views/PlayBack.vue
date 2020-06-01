f7aaf173209d301c53ecdc4494fedd99ad04bda2
Simone <sim.pietrogiacomi@gmail.com>
2020-06-01 12:18:47 +0200

    Fixed reading playback data from backend

--- client/src/views/PlayBack.vue
+++ client/src/views/PlayBack.vue
@@ -31,12 +31,12 @@ export default {
   async created () {
     try {
       const data = await GroupService.getVisitGroup()
-      const PoICoordinates = {}
-      for (const PoI of data.mapData) {
-        PoICoordinates[PoI.name] = [PoI.x, PoI.y]
-      }
-      this.mapData = PoICoordinates
-      // this.mapData = data.mapData
+      // const PoICoordinates = {}
+      // for (const PoI of data.mapData) {
+      //   PoICoordinates[PoI.name] = [PoI.x, PoI.y]
+      // }
+      // this.mapData = PoICoordinates
+      this.mapData = data.mapData
       this.visitorsData = data.visitorsData
       this.groupList = this.visitorsData.map(visitorData => visitorData.groupID)
       // Cut group duplicates
@@ -74,10 +74,10 @@ export default {
           // TODO: Change "visitRow" name, it's terribly ugly
           const visitRow = [
             visitorPosition.startTime,
-            this.mapData[exhibit][0],
-            this.mapData[exhibit][1],
-            // this.mapData[exhibit].x,
-            // this.mapData[exhibit].y,
+            // this.mapData[exhibit][0],
+            // this.mapData[exhibit][1],
+            this.mapData[exhibit].x,
+            this.mapData[exhibit].y,
             selectedVisitorsList[visitorPositionsIndex],
             exhibit
           ]
