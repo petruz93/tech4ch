7d2b8fd56425b579dded6c2ffe921a778a1434a2
Matteo Petruzziello <matteop93@hotmail.it>
2020-06-01 03:20:28 +0200

    controllers fetches from db

--- server/src/controllers/pointOfInterestController.js
+++ server/src/controllers/pointOfInterestController.js
@@ -2,12 +2,30 @@ const express = require('express');
 // const path = require('path');
 // const fs = require('fs');
 // const xml2js = require('xml2js');
-const mapData = require('../files/map-data');
+// const mapData = require('../files/map-data');
+const Poi = require('../models/pointOfInterest');
 
 const router = express.Router();
 
-const getMapData = (req, res) => {
-  res.json(mapData);
+function pois2map(pois) {
+  const result = {};
+  pois.forEach((p) => {
+    result[p.name] = {
+      x: p.x,
+      y: p.y,
+      room: p.room
+    };
+  });
+  return result;
+}
+
+const getMapData = (req, res, next) => {
+  const query = Poi.find({}, '-_id -__v');
+  query.exec((err, pois) => {
+    if (err) next(err);
+    res.json(pois2map(pois));
+    // return pois2map(pois);
+  });
 };
 
 // const getMapDataJson = (req, res) => {
--- server/src/controllers/visitorController.js
+++ server/src/controllers/visitorController.js
@@ -1,40 +1,44 @@
 const express = require('express');
-const path = require('path');
-const parser = require('../utility/csvParser');
-const mapData = require('../files/map-data');
+// const path = require('path');
+// const parser = require('../utility/csvParser');
+// const mapData = require('../files/map-data');
+const Visitor = require('../models/visitor');
 
 const router = express.Router();
 
-const getVisitor = (req, res, next) => {
-  const filepath = path.join(__dirname, '../files/visitors-log/visitor_342_257.csv');
-  parser.csv2visitor(filepath)
-    .then((value) => res.json(value))
-    .catch((err) => next(err));
-};
+// const getVisitor = (req, res, next) => {
+//   const filepath = path.join(__dirname, '../files/visitors-log/visitor_342_257.csv');
+//   parser.csv2visitor(filepath)
+//     .then((value) => res.json(value))
+//     .catch((err) => next(err));
+// };
+
+// const getAllVisitors = async (req, res, next) => {
+//   const dirpath = path.join(__dirname, '../files/visitors-log/');
+//   try {
+//     const visitorsData = await parser.readAllVisitorsFromDir(dirpath);
+//     res.json(visitorsData);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
 
 const getAllVisitors = async (req, res, next) => {
-  const dirpath = path.join(__dirname, '../files/visitors-log/');
-  try {
-    const visitorsData = await parser.readAllVisitorsFromDir(dirpath);
-    res.json(visitorsData);
-  } catch (err) {
-    next(err);
-  }
+  const query = Visitor.find({}, '-_id -__v');
+  query.exec((err, visitors) => {
+    if (err) next(err);
+    // return visitors;
+    res.json(visitors);
+  });
 };
 
-const getAllVisitorsAndMap = async (req, res, next) => {
-  const dirpath = path.join(__dirname, '../files/visitors-log/');
-  try {
-    const visitorsData = await parser.readAllVisitorsFromDir(dirpath);
-    res.json({ visitorsData, mapData });
-  } catch (err) {
-    next(err);
-  }
-};
+// const getAllVisitorsAndMap = async (req, res, next) => {
+//   const q1 = Visitor.find();
+// };
 
 
-router.get('/visitors/test', getVisitor);
+// router.get('/visitors/test', getVisitor);
 router.get('/visitors/all', getAllVisitors);
-router.get('/alldata', getAllVisitorsAndMap);
+// router.get('/alldata', getAllVisitorsAndMap);
 
 module.exports = router;
